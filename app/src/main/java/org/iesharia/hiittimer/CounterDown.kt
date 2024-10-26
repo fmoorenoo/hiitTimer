7ef255d2289b5d26bf7cf1c1ce0fcfbaef39b805
fmoorenoo <fermontemore@gmail.com>
2024-10-26 12:26:59 +0100

    Nodificado CounterDown.kt y añadidas funciones de pausar y reanudar

--- app/src/main/java/org/iesharia/hiittimer/CounterDown.kt
+++ app/src/main/java/org/iesharia/hiittimer/CounterDown.kt
@@ -3,22 +3,39 @@ package org.iesharia.hiittimer
 import android.os.CountDownTimer
 
 class CounterDown(var segundos: Int, var loquehacealhacertick: (Long) -> Unit) {
-    private var counterState : Boolean = false
-    val myCounter = object : CountDownTimer((segundos * 1000L), 1000) {
-        override fun onTick(millisUntilFinished: Long) {
-            if (counterState) loquehacealhacertick(millisUntilFinished / 1000)
-        }
-        override fun onFinish() {
-            counterState = false
-        }
-    }
+    private var counterState: Boolean = false
+    private var restante: Long = segundos * 1000L
+    private var myCounter: CountDownTimer? = null
 
     fun start() {
         counterState = true
-        this.myCounter.start()
+        myCounter = object : CountDownTimer(restante, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                if (counterState) {
+                    restante = millisUntilFinished
+                    loquehacealhacertick(millisUntilFinished / 1000)
+                }
+            }
+
+            override fun onFinish() {
+                counterState = false
+                loquehacealhacertick(0)
+            }
+        }.start()
     }
+
+    fun pause() {
+        counterState = false
+        myCounter?.cancel()
+    }
+
+    fun resume() {
+        counterState = true
+        start()
+    }
+
     fun cancel() {
         counterState = false
-        this.myCounter.cancel()
+        myCounter?.cancel()
     }
 }
